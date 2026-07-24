19c993cde5bf886eb5eb0ebc43d29bb1bb735847
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2026-07-24 22:49:36 +0900

    [D1] Title: 최대수 구하기, Time: 88 ms, Memory: 26,496 KB -BaekjoonHub

---
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            int max = 0;
+            for (int i = 0; i < 10; i++) {
+            	int num = sc.nextInt();
+                if (num > max) {
+                    max = num;
+            	}
+			}
+            System.out.println("#" + test_case + " " + max);
+		}
+	}
+}    
