08687b48dd1f70da817ccb00d55cb88438a8d4ea
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2026-07-24 22:58:49 +0900

    [D1] Title: 간단한 N 의 약수, Time: 97 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for (int i = 1; i < N+1; i++){
+        	if (N % i == 0) {
+            	System.out.print(i + " ");
+            }
+        }
+	}
+}
