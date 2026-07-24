cba003feea35986b74ebc7b0a1eed0dd054cccb0
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2026-07-24 22:50:33 +0900

    [D1] Title: 신문 헤드라인, Time: 87 ms, Memory: 26,880 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.toUpperCase());
+	}
+}
