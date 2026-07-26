37da33453b9f182544637e6561c78c960257176f
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2026-07-26 21:31:17 +0900

    [D2] Title: 파리 퇴치, Time: 123 ms, Memory: 29,880 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,47 +1,50 @@
 import java.util.Scanner;
-
-class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-
-        for (int test_case = 1; test_case <= T; test_case++) {
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            // 배열 크기 받는다.
             int N = sc.nextInt();
+            // 파리채 크기 받는다.
             int M = sc.nextInt();
-
-            // N x N 크기의 배열 선언
+            
+            // 파리 배열 선언한다.
             int[][] flyArr = new int[N][N];
-
-            // 배열 입력
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
+            
+            // 각 배열 칸에 파리 수 받는다.
+            for(int i = 0; i < N; i++) {
+            	for (int j = 0; j < N; j++) {
                     flyArr[i][j] = sc.nextInt();
                 }
             }
-
+            
+            // 최대 파리 수 값 초기화
             int maxSum = 0;
-
-            // 파리채의 왼쪽 위 시작점을 선택
-            for (int i = 0; i <= N - M; i++) {
-                for (int j = 0; j <= N - M; j++) {
-
+            
+            // 각 칸을 파리채 왼쪽 위 시작점으로 탐색하기 위한 반복문
+            for (int i = 0; i <= N-M; i++) {
+            	for (int j = 0; j <= N-M; j++) {
+                	
+                    // 각각의 파리채 크키&위치별 파리 수 합 초기화
                     int currentSum = 0;
-
-                    // 시작점부터 M x M 영역의 모든 값을 더함
-                    for (int row = i; row < i + M; row++) {
-                        for (int col = j; col < j + M; col++) {
-                            currentSum += flyArr[row][col];
+                    
+                    // 파리 수 더하기 위한 반복문
+                    for (int k = 0; k < M; k++) {
+                    	for (int l = 0; l < M; l++) {
+                            currentSum += flyArr[i + k][j + l];
                         }
                     }
-
-                    // 지금까지의 최댓값과 비교
+                    // 최대인지 비교
                     maxSum = Math.max(maxSum, currentSum);
                 }
             }
-
+            
             System.out.println("#" + test_case + " " + maxSum);
-        }
-
-        sc.close();
-    }
+		}
+	}
 }
