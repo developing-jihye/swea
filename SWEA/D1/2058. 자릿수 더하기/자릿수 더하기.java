404261fff26a74d06c811f526e8cacaf59117487
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2026-08-18 16:36:35 +0900

    [D1] Title: 자릿수 더하기, Time: 90 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
 class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int sum = 0;
-        for (int i = 0; i < N; i++){
-        	int num = N%10;
-            sum += num;
-            N =N/10;
+		int N = sc.nextInt();
+        
+		int sum = 0;
+        while(N > 0) {
+        	sum += N % 10;
+        	N = N / 10;
         }
         System.out.println(sum);
 	}
