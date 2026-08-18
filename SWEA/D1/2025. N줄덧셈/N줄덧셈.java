8656a51d234a2a96f880d256425c320a8358b0e9
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2026-08-18 16:41:01 +0900

    [D1] Title: N줄덧셈, Time: 89 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D1/2025. N줄덧셈/N줄덧셈.java
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -5,13 +5,13 @@ class Solution
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+		int N = sc.nextInt();
         
-        int sum = 0;
-        
-        for (int i = 1; i < N+1; i++) {
-            sum += i;
-        }
+		int sum = 0;
+		
+		for(int i = 1; i <= N; i++) {
+			sum += i;
+		}
         
         System.out.println(sum);
 	}
