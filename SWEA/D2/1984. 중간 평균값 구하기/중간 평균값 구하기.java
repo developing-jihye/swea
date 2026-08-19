033c9afad090112d7f110a9790954f44f790d81a
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2026-08-19 17:21:26 +0900

    [D2] Title: 중간 평균값 구하기, Time: 93 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -1,42 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
+class Solution{
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			int[] nums = new int[10];
-			for (int i = 0; i < 10;i++) {
-				nums[i] = sc.nextInt();
-			}
+		int T = sc.nextInt();
 		
-			int min = nums[0];
-			int max = nums[0];
-			for(int n: nums) {
-				if (n > max) {
-					max = n; 
-				}
-				
-				if(n < min) {
-					min = n;
-				}
+		for (int test_case = 1; test_case <= T; test_case++) {
+			
+			int[] arr = new int[10];
+			for (int i = 0; i < 10; i++) {
+				arr[i] = sc.nextInt();
 			}
+			
+			Arrays.sort(arr);
 			
 			int sum = 0;
-			for(int n: nums) {
-				sum += n;
+			for (int i = 1; i < 9; i++) {
+				sum += arr[i];
 			}
-			sum -= min;
-			sum -= max;
 			
-			double avg = (double) sum / 8;
+			double result = (double) sum / 8;
 			
-			System.out.println("#" + test_case + " " + Math.round(avg));
+			System.out.println("#" + test_case + " " + Math.round(result));
 		}
 	}
 }
